9d9c70fcb54d33a141a5dbad0fd63619af407935
hridoykhan <redoykhan01943147885@gmail.com>
2024-06-06 11:23:34 +0600

    commit is here

---
+++ operator.js
@@ -0,0 +1,9 @@
+var a = 7;
+var b = 9;
+var c = a + b;
+document.write("add:" + c + "<br>");
+document.write("sub:" +(a - b) + "<br>");
+document.write("mul:" + a * b + "<br>");
+document.write("div:" + c / a + "<br>");
+document.write("remainder:" + a % b + "<br>");
+document.write("remainder:" + a ** b  + "<br>");
